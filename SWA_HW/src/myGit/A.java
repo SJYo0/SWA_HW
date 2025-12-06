145c81ac0e4e153dc9d65be6947e06382ca86e1b
SJYo0 <wnsdyd9109@gmail.com>
2025-12-06 18:34:57 +0900

    Bob: A-메뉴선택 B / B-메뉴변경(마라탕)

--- SWA_HW/src/myGit/A.java
+++ SWA_HW/src/myGit/A.java
@@ -4,7 +4,10 @@ public class A {
 
 	public static void main(String[] args) {
 		System.out.println("난 저녁으로 무엇을 먹을까?");
-
+		
+		B selectMenuB = new B();
+		
+		System.out.println("메뉴 선택 : "+selectMenuB.getMenu());
 	}
 
 }
